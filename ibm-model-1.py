708910160d5139a253cb64c28cabe940b08b47c6
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-02-26 01:36:25 -0800

    Re-implements self.init_transl_probs() with list of lists instead of dictionary of dictionaries

--- ibm-model-1.py
+++ ibm-model-1.py
@@ -12,6 +12,8 @@ from datetime import datetime
 
 pp = pprint.PrettyPrinter(indent=3)
 PATH_TO_TRAIN = './es-en/train/'
+PATH_TO_DEV = './es-en/dev/'
+FILENAME = 'europarl-v7.es-en'
 UTF_SPECIAL_CHARS = {
   '\\xc2\\xa1' : '',
   '\\xc2\\xbf' : '',
@@ -73,62 +75,63 @@ UTF_SPECIAL_CHARS = {
 class M1:
   # IBM Model1 initialization
   def __init__(self):
-    #Isolate pairings from documents 
-    self.en_vocab = set()
-    self.sp_vocab = set()
+    sp_doc = get_lines_of_file('%s%s.es' % (PATH_TO_TRAIN, FILENAME))
+    en_doc = get_lines_of_file('%s%s.en' % (PATH_TO_TRAIN, FILENAME))
+    sentence_pairs = self.deconstuct_sentences(sp_doc, en_doc)
 
-    sp_doc = get_lines_of_file('%seuroparl-v7.es-en.es' % (PATH_TO_TRAIN))
-    # sp_doc = ["Yo tengo un perro", "Yo tengo"]
-    en_doc = get_lines_of_file('%seuroparl-v7.es-en.en' % (PATH_TO_TRAIN))
-    # en_doc = ["I have a dog", "I have"]
+    ##
+    # Initialize transl_probs uniformly. It's table from spanish words to
+    # english words (list of lists) to probability of that english word being
+    # the correct translation. Every translation probability is
+    # initialized to (1/# english words) since every word is equally
+    # likely to be the correct translation.
+    self.init_transl_probs()
 
-    sentence_pairs = self.get_sentence_pairs(sp_doc, en_doc)
+    # self.transl_probs = self.find_probabilities()
 
-    # ##
-    # # Initialize transl_probs uniformly (hash from spanish words to hash from english words
-    # # to probability of that english word beign the correct translation. Every translation
-    # # probability is initialized to 1/#english words since every word is equally likely to 
-    # # be the correct translation.)
-    self.transl_probs = self.find_probabilities()
-
-    print 'completed transl_probs'
-    #Initialize counts and totals to be used in main loop. 
+    # print 'initialized self.transl_probs'
+    # #Initialize counts and totals to be used in main loop. 
    
-    #MASSIVE LOOPDELOOP
-    #create the counts hash
-    temp = dict.fromkeys(self.en_vocab, 0)
-    self.counts = {}
-    for key in self.sp_vocab:
-      self.counts[key] = copy.deepcopy(temp)
-    #self.counts = dict.fromkeys(copy.deepcopy(self.sp_vocab), copy.deepcopy(temp))
-    
-    self.total_s = dict.fromkeys(self.sp_vocab, 0)
-    for pair in sentence_pairs:
-      self.total_e = dict.fromkeys(self.en_vocab, 0)
-      sp_sentence = pair[0]
-      en_sentence = pair[1]
-      for english_word in en_sentence.split():
-        for spanish_word in sp_sentence.split():
-          self.total_e[english_word] += self.transl_probs[spanish_word][english_word]
-      for english_word in en_sentence.split():
-        for spanish_word in sp_sentence.split():
-          self.counts[spanish_word][english_word] += (self.transl_probs[spanish_word][english_word] *1.0/ self.total_e[english_word])
-          self.total_s[spanish_word] += (self.transl_probs[spanish_word][english_word] / self.total_e[english_word])
+    # # MASSIVE LOOPDELOOP
+    # # create the counts hash
+    # temp = dict.fromkeys(self.en_vocab, 0)
+    # self.counts = {}
+    # for key in self.sp_vocab:
+    #   self.counts[key] = copy.deepcopy(temp)
 
-    for spanish_word in self.sp_vocab:
-      for english_word in self.en_vocab:
-        self.transl_probs[spanish_word][english_word] = (self.counts[spanish_word][english_word] / self.total_s[spanish_word])
-        print spanish_word + ":" + english_word + ":" + str(self.transl_probs[spanish_word][english_word])
+    # print 'initialized self.counts'
 
-    #PRint out highest probability pairs: NOT PART OF ALGORITHM
-    for spanish_word in self.transl_probs.keys():
-      max_prob_engligh_word = "poop"
-      max_prob = 0
-      for english_word in self.transl_probs[spanish_word].keys():
-        if self.transl_probs[spanish_word][english_word] > max_prob:
-          max_prob = self.transl_probs[spanish_word][english_word]
-          max_prob_engligh_word = english_word
-      print spanish_word + ":" + max_prob_engligh_word + ":" + str(max_prob)
+    # algoriesm
+      # self.total_s = dict.fromkeys(self.sp_vocab, 0)
+      # for pair in sentence_pairs:
+      #   self.total_e = dict.fromkeys(self.en_vocab, 0)
+      #   sp_sentence = pair[0]
+      #   en_sentence = pair[1]
+      #   for english_word in en_sentence.split():
+      #     for spanish_word in sp_sentence.split():
+      #       self.total_e[english_word] += self.transl_probs[spanish_word][english_word]
+      #   for english_word in en_sentence.split():
+      #     for spanish_word in sp_sentence.split():
+      #       self.counts[spanish_word][english_word] += (self.transl_probs[spanish_word][english_word] / (self.total_e[english_word]*1.0))
+      #       self.total_s[spanish_word] += (self.transl_probs[spanish_word][english_word] / (self.total_e[english_word]*1.0))
+
+      # for spanish_word in self.sp_vocab:
+      #   for english_word in self.en_vocab:
+      #     if self.total_s[spanish_word] == 0:
+      #       self.transl_probs[spanish_word][english_word] = 0
+      #     else:
+      #       self.transl_probs[spanish_word][english_word] = (self.counts[spanish_word][english_word] / (self.total_s[spanish_word]*1.0))
+      #     # print spanish_word + ":" + english_word + ":" + str(self.transl_probs[spanish_word][english_word])
+
+      # # Print out highest probability pairs: NOT PART OF ALGORITHM
+      # for spanish_word in self.transl_probs.keys():
+      #   max_prob_engligh_word = "poop"
+      #   max_prob = 0
+      #   for english_word in self.transl_probs[spanish_word].keys():
+      #     if self.transl_probs[spanish_word][english_word] > max_prob:
+      #       max_prob = self.transl_probs[spanish_word][english_word]
+      #       max_prob_engligh_word = english_word
+      #   print spanish_word + ":" + max_prob_engligh_word + "   " + str(max_prob)
 
 
   def find_probabilities(self):
@@ -137,21 +140,52 @@ class M1:
     for key in self.sp_vocab:
       temp_dict[key] = copy.deepcopy(temp)
     return temp_dict
-    #return dict.fromkeys(self.sp_vocab, dict.fromkeys(self.en_vocab, 1.0/len(self.en_vocab)))
 
-  #takes in an array of sentences of sp and en words
-  #returns tuples in the form of (sp sentence, en sentence)
-  def get_sentence_pairs(self, sp_doc, en_doc):
-    tuples = []
+  def init_transl_probs(self):
+    num_english_words = len(self.en_vocab_list)
+    temp = [1.0/num_english_words] * num_english_words
+    for i in range(0, len(self.sp_vocab_list)):
+      self.sp_vocab_list[i] = temp[0:]
+
+  ##
+  # Takes in an array of sentences of sp and en words
+  # returns tuples in the form of (sp sentence, en sentence)
+  def deconstuct_sentences(self, sp_doc, en_doc):
+    self.en_vocab_indices = {}
+    self.sp_vocab_indices = {}
+
+    ##
+    # Iterate through all English & Spanish sentences & add each word
+    # to the respective vocabularies.
+    en_vocab = set()
+    sp_vocab = set()
     for en_sentence in en_doc:
       for en_word in en_sentence.split(' '):
-        self.en_vocab.add(en_word)
+        en_vocab.add(en_word)
     for sp_sentence in sp_doc:
       for sp_word in sp_sentence.split(' '):
-        self.sp_vocab.add(sp_word)
-      
+        sp_vocab.add(sp_word)
+        
+    # Build sorted vocab list.
+    self.en_vocab_list = list(sorted(en_vocab))
+    self.sp_vocab_list = list(sorted(sp_vocab))
+
+    ##
+    # Build dict for each vocabulary in which keys are words and their
+    # values are their respective indices.
+    for i in range(0, len(self.en_vocab_list)):
+      word = self.en_vocab_list[i]
+      self.en_vocab_indices[word] = i
+    for i in range(0, len(self.sp_vocab_list)):
+      word = self.sp_vocab_list[i]
+      self.sp_vocab_indices[word] = i
+  
+    # Build list of sentence pair tuples.
+    tuples = []
     for i, sp_sentence in enumerate(sp_doc):
       tuples.append((sp_doc[i], en_doc[i]))
+
+    # Return list of sentence pair tuples.
     return tuples
 
 
@@ -192,12 +226,11 @@ def get_lines_of_file(fileName):
 
 def main():
   m = M1()
-  # m.find_probabilities()
-  
+
 
 if __name__ == "__main__":
   startTime = datetime.now()
   main()
-  print datetime.now() - startTime
+  print 'Time elapsed:   %s' % (str(datetime.now() - startTime))
 
   
