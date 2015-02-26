a3939445184aa5a8de5b8d5744a13789ecf6bfe6
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-02-25 18:55:20 -0800

    Adds cache functionality for translation probabilities

--- ibm-model-1.py
+++ ibm-model-1.py
@@ -7,27 +7,35 @@ import math
 import collections
 import copy
 import re
+import pickle
 import pprint
 
 pp = pprint.PrettyPrinter(indent=3)
-PATH_TO_DEV = './es-en/dev/'
-UTF_SPECIAL_CHARS = {
+
+##
+# TODO: implement this so that when False, it doesn't fill the
+# cache file. Allow this to be a flag!!
+CACHE_FILENAME = 'transl_probs.pickle'
+USE_CACHE = True
+
+path_to_dev = './es-en/dev/'
+utf_special_chars = {
   '\\xc2\\xa1' : '',
   '\\xc2\\xbf' : '',
-  '\\xc3\\x81' : 'A',
-  '\\xc3\\x89' : 'E',
-  '\\xc3\\x8d' : 'I',
-  '\\xc3\\x91' : 'N',
-  '\\xc3\\x93' : 'O',
-  '\\xc3\\x9a' : 'U',
-  '\\xc3\\x9c' : 'U',
-  '\\xc3\\xa1' : 'A',
-  '\\xc3\\xa9' : 'E',
-  '\\xc3\\xad' : 'I',
-  '\\xc3\\xb1' : 'N',
-  '\\xc3\\xb3' : 'O',
-  '\\xc3\\xba' : 'U',
-  '\\xc3\\xbc' : 'U',
+  '\\xc3\\x81' : 'a',
+  '\\xc3\\x89' : 'e',
+  '\\xc3\\x8d' : 'i',
+  '\\xc3\\x91' : 'n',
+  '\\xc3\\x93' : 'o',
+  '\\xc3\\x9a' : 'u',
+  '\\xc3\\x9c' : 'u',
+  '\\xc3\\xa1' : 'a',
+  '\\xc3\\xa9' : 'e',
+  '\\xc3\\xad' : 'i',
+  '\\xc3\\xb1' : 'n',
+  '\\xc3\\xb3' : 'o',
+  '\\xc3\\xba' : 'u',
+  '\\xc3\\xbc' : 'u',
   '\'' : '',
   '\\n' : '',
   '&quot;' : ''
@@ -51,48 +59,50 @@ UTF_SPECIAL_CHARS = {
   #     for all e
   #       transl_prob(e|f) = count(e|f) / total(f)
 
-# Values for english within spanish word must sum to 1.0
+# values for english within spanish word must sum to 1.0
   # {
-  #   'Yo': {
-  #     'I': .3
+  #   'yo': {
+  #     'i': .3
   #     'have': .4
   #     'dog': .3
   #   }
   #   'tengo': {
-  #     'I': .3
+  #     'i': .3
   #     'have': .4
   #     'dog': .3
   #   }
   #   'perro': {
-  #     'I': .3
+  #     'i': .3
   #     'have': .4
   #     'dog': .3
   #   }
   # }
-class M1:
-  # IBM Model1 initialization
+class m1:
+  # ibm model1 initialization
   def __init__(self):
-    #Isolate pairings from documents 
+    #isolate pairings from documents 
     self.en_vocab = set()
     self.sp_vocab = set()
 
-    sp_doc = get_lines_of_file('%snewstest2012.es' % (PATH_TO_DEV))
-    # sp_doc = ["Yo tengo un perro", "Yo tengo"]
-    en_doc = get_lines_of_file('%snewstest2012.en' % (PATH_TO_DEV))
-    # en_doc = ["I have a dog", "I have"]
+    sp_doc = get_lines_of_file('%snewstest2012.es' % (path_to_dev))
+    # sp_doc = ["yo tengo un perro", "yo tengo"]
+    en_doc = get_lines_of_file('%snewstest2012.en' % (path_to_dev))
+    # en_doc = ["i have a dog", "i have"]
 
     sentence_pairs = self.get_sentence_pairs(sp_doc, en_doc)
 
     ##
-    # Initialize transl_probs uniformly (hash from spanish words to hash from english words
-    # to probability of that english word beign the correct translation. Every translation
+    # initialize transl_probs uniformly (hash from spanish words to hash from english words
+    # to probability of that english word beign the correct translation. every translation
     # probability is initialized to 1/#english words since every word is equally likely to 
     # be the correct translation.)
+    print '------------ CALCULATING TRALSATION PROBABILITIES .... ------------'
     self.transl_probs = self.find_probabilities()
+    print '------------ DONE CALCULATING TRALSATION PROBABILITIES ------------'
 
-    #Initialize counts and totals to be used in main loop. 
+    #initialize counts and totals to be used in main loop. 
    
-    # MASSIVE LOOPDELOOP
+    # massive loopdeloop
     # create the counts hash
     temp = dict.fromkeys(self.en_vocab, 0)
     self.counts = {}
@@ -117,7 +127,7 @@ class M1:
       for english_word in self.en_vocab:
         self.transl_probs[spanish_word][english_word] = (self.counts[spanish_word][english_word] / self.total_s[spanish_word])
 
-    #PRint out highest probability pairs: NOT PART OF ALGORITHM
+    #print out highest probability pairs: not part of algorithm
     for spanish_word in self.transl_probs.keys():
       max_prob_engligh_word = "poop"
       max_prob = 0
@@ -129,12 +139,23 @@ class M1:
 
 
   def find_probabilities(self):
-    temp = dict.fromkeys(self.en_vocab, 1.0/len(self.en_vocab))
-    temp_dict = {}
-    for key in self.sp_vocab:
-      temp_dict[key] = copy.deepcopy(temp)
-    return temp_dict
-    #return dict.fromkeys(self.sp_vocab, dict.fromkeys(self.en_vocab, 1.0/len(self.en_vocab)))
+    if os.path.exists(CACHE_FILENAME):
+      print 'Cache file exists!'
+      with open(CACHE_FILENAME, "rb") as f:
+        return pickle.load(f)
+    else:
+      print 'Cache file does not yet exist. Building now...'
+
+      temp = dict.fromkeys(self.en_vocab, 1.0/len(self.en_vocab))
+      temp_dict = {}
+      for key in self.sp_vocab:
+        temp_dict[key] = copy.deepcopy(temp)
+      
+      # Save into file
+      with open(CACHE_FILENAME, 'wb') as f:
+        pickle.dump(temp_dict, f)
+
+      return temp_dict
 
   #takes in an array of sentences of sp and en words
   #returns tuples in the form of (sp sentence, en sentence)
@@ -153,46 +174,42 @@ class M1:
 
 
 ##
-# Code for reading a file.  you probably don't want to modify anything here, 
+# code for reading a file.  you probably don't want to modify anything here, 
 # unless you don't like the way we segment files.
-def get_lines_of_file(fileName):
+def get_lines_of_file(filename):
   lines = []
-  with open(fileName,'r') as f: 
+  with open(filename,'r') as f: 
     for line in f:
       ##
-      # First we lowercase the line in order to treat capitalized
+      # first we lowercase the line in order to treat capitalized
       # and non-capitalized instances of a single word the same.
       ##
-      # Then, repr() forces the output into a string literal UTF-8
+      # then, repr() forces the output into a string literal utf-8
       # format, with characters such as '\xc3\x8d' representing
-      # special characters not found in typical ASCII.
+      # special characters not found in typical ascii.
       line = repr(line.lower())
       
       ##
-      # Replace all instances of UTF-8 character codes with
-      # uppercase letters of the nearest ASCII equivalent. For
-      # instance, 'á' becomes '\\xc3\\xa1' becomes 'A'. The
+      # replace all instances of utf-8 character codes with
+      # uppercase letters of the nearest ascii equivalent. for
+      # instance, 'á' becomes '\\xc3\\xa1' becomes 'a'. the
       # purpose of making these special characters uppercase is
       # to differentiate them from the rest of the non-special
       # characters, which are all lowercase.
-      for utf8_code, replacement_char in UTF_SPECIAL_CHARS.items():
+      for utf8_code, replacement_char in utf_special_chars.items():
         line = line.replace(utf8_code, replacement_char)
       
-      # Remove any non-whitespace, non-alphabetic characters.
-      line = re.sub(r'[^A-z ]', '', line)
+      # remove any non-whitespace, non-alphabetic characters.
+      line = re.sub(r'[^a-z ]', '', line)
       
-      # Substitute multiple whitespace with single whitespace, then
+      # substitute multiple whitespace with single whitespace, then
       # append the cleaned line to the list.
       lines.append(' '.join(line.split()))
   return lines
 
 
 def main():
-  m = M1()
-  # m.find_probabilities()
-  
+  m = m1()  
 
 if __name__ == "__main__":
   main()
-
-  
