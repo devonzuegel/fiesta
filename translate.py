c063fc051c7a41ff66c50731bc2753b2c46222d6
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-02-26 16:58:21 -0800

    Adds single-sentence translation.

--- translate.py
+++ translate.py
@@ -22,10 +22,12 @@ FILENAME = 'test2'
 def main():
   m1 = M1()
   sp_sentence = 'Ahora bien , se ha demostrado que no es así .'.split()
+  en_translation = ''
   for sp_word in sp_sentence:
-    top_translation = m1.top_english_word(sp_word)
-    print top_translation
-
+    en_translation += ' %s' % m1.top_english_word(sp_word)
+  print ' '.join(sp_sentence)
+  print en_translation
+  print 'Goal: "However, at this stage these are just proposals."'
 if __name__ == "__main__":
   startTime = datetime.now()
   main()
