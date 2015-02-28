8262e2d82064b559b35a999fcc6a30f169768934
zrobert7 <zrobert7@stanford.edu>
2015-02-27 17:12:16 -0800

    fuck

--- translate.py
+++ translate.py
@@ -82,11 +82,13 @@ def main():
     possible_arrangements = rearrange_sentence(best_sentence)
     for sentence in possible_arrangements:
       #print sentence
-      bigram_score = 0
+      bigram_score = 0.0
       for i in range(num_words_in_trans_sentence):
         if i >= 2:
-          bigram_e = sentence[i-2] + ' ' + sentence[i-1]
-          bigram_score += m1.get_bigram_probability(bigram_e, sentence[i])
+          bigram_e = sentence[i-1]
+          temp = m1.get_bigram_probability(bigram_e, sentence[i])
+          #print temp
+          bigram_score += temp
       if bigram_score > max_bigram_score:
         max_bigram_score = bigram_score
         best_sentence = sentence
